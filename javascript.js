8b62f799c4f78346e5500563f63eb6d14d8c25f7
Carrie <thelongdinosaur@gmail.com>
2018-03-17 13:17:45 -0400

    refining syntax

--- javascript.js
+++ javascript.js
@@ -1,23 +1,24 @@
 
-$("#keyWordsButton").on("click", function(event) {
-    var keywords = $("#keywords").val();
+    $("#keyWordsButton").on("click", function(event) {
+        var keywords = $("#keywords").val();
+        event.preventDefault();
+    })
 
-event.preventDefault();
+    $("#yearButton").on("click", function(event) {
+        var year = $("#year").val();
+        event.preventDefault();
+    })
 
-$("#yearButton").on("click", function(event)) {
-    var year = $("#year").val();
-}
-
-var url = "https://api.nytimes.com/svc/archive/v1/2016/1.json";
-url += '?' + $.param({
-    'api-key':"a37f87b8092245b5b1e6d60c602534f0"
-});
-$.ajax({
-  url: url,
-  method: 'GET',
-}).done(function(result) {
-  console.log(result);
-}).fail(function(err) {
-  throw err;
-});
+    var url = "https://api.nytimes.com/svc/archive/v1/2016/1.json";
+    url += '?' + $.param({
+        'api-key':"a37f87b8092245b5b1e6d60c602534f0"
+    });
+    $.ajax({
+    url: url,
+    method: 'GET',
+    }).done(function(result) {
+    console.log(result);
+    }).fail(function(err) {
+    throw err;
+    })
 
